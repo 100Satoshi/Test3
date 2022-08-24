55ec07332f828a1865597c4cb34e58c71f8ef7e7
100satoshi <tofil.kaszewiak.patryk@gmail.com>
2022-08-24 18:13:41 +0200

    poprawione mapowania

--- src/main/java/com/Test3/controller/ExchangeController.java
+++ src/main/java/com/Test3/controller/ExchangeController.java
@@ -5,25 +5,23 @@ import com.Test3.model.ExchangeStats;
 import com.Test3.model.Query;
 import com.Test3.service.ExchangeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/exchange")
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
 
-    @PostMapping("/exchange/query")
+    @PostMapping("/query")
     public ConvertModelData exchange(@Valid @RequestBody Query query) {
         return exchangeService.exchange(query);
     }
 
-    @GetMapping("/exchange/stats")
+    @GetMapping("/stats")
     public ExchangeStats exchageStats() {
         return exchangeService.exchangeStats();
     }
